be5c731cc8ac0c556aaed5a02a8de8a4f52345af
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2017-12-22 09:45:15 +0800

    Initial finished files

--- FadeInFadeOut/ViewController.swift
+++ FadeInFadeOut/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var buton: UIButton!
+    @IBOutlet weak var segControl: UISegmentedControl!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func fadeIn(_ sender: Any) {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.label.alpha = 1
+            self.buton.alpha = 1
+            self.segControl.alpha = 1
+        })
+    }
+    
+    @IBAction func fadeOut(_ sender: Any) {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.label.alpha = 0
+            self.buton.alpha = 0
+            self.segControl.alpha = 0
+        })
+    }
+    
+    @IBAction func alphaSlider(_ sender: Any) {
+        label.alpha = CGFloat(slider.value)
+        buton.alpha = CGFloat(slider.value)
+        segControl.alpha = CGFloat(slider.value)
+    }
+    
 }
 
